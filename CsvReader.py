c59207415a6026cff14a20b8d8c92b2f83dc63c8
Niall French <nfrench01@qub.ac.uk>
2018-08-24 12:31:53 +0100

    Missing file raises ValueError

--- CsvReader.py
+++ CsvReader.py
@@ -1,3 +1,7 @@
+import typing
+
+
 class CsvReader:
-    def __init__(self):
-        pass
+    def __init__(self, delimiter: str=',', quote_char: str='"', header_row: bool=True, file: typing.TextIO=None):
+        if file is None:
+            raise ValueError('You must provide a file to parse.')
